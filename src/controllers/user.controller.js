d9e18393ad7e6136c4a2e6e2896821237da0726f
MdHasib01 <md.hasibuzzaman001@gmail.com>
2024-02-16 02:03:35 +0600

    register controller updated

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -6,7 +6,7 @@ import { ApiResponse } from "../utils/ApiResponse.js";
 
 const registerUser = asyncHandler(async (req, res) => {
   const { fullName, email, username, password } = req.body;
-  console.log("email:", email);
+  // console.log("email:", email);
 
   //Checking if any field is empty
   if (
@@ -16,7 +16,7 @@ const registerUser = asyncHandler(async (req, res) => {
   }
 
   //Checking if user and email are already in the database or not
-  const existedUser = User.findOne({
+  const existedUser = await User.findOne({
     $or: [{ username }, { email }],
   });
 
@@ -26,7 +26,16 @@ const registerUser = asyncHandler(async (req, res) => {
 
   //Checking if the files uploaded in the server or not
   const avatarLocalPath = req.files?.avatar[0]?.path;
-  const coverImageLocalPath = req.files?.coverImage[0]?.path;
+  // const coverImageLocalPath = req.files?.coverImage[0]?.path;
+
+  let coverImageLocalPath;
+  if (
+    req.files &&
+    Array.isArray(req.files.coverImage) &&
+    req.files.coverImage > 0
+  ) {
+    coverImageLocalPath = req.files.coverImage[0].path;
+  }
 
   if (!avatarLocalPath) {
     throw new ApiError(400, "avatar file is required");
@@ -42,27 +51,27 @@ const registerUser = asyncHandler(async (req, res) => {
   }
 
   //Creating user
- const user = await User.create({
-   fullName,
-   avatar: avatar.url,
-   coverImage: coverImage?.url || "",
-   email,
-   password,
-   username: username.toLowercase(),
- });
+  const user = await User.create({
+    fullName,
+    avatar: avatar.url,
+    coverImage: coverImage?.url || "",
+    email,
+    password,
+    username: username.toLowerCase(),
+  });
 
- //Taking out some data for safety from the frontend response
- const createdUser = await User.findById(user._id).select(
-   "-password -refreshToken"
- );
+  //Taking out some data for safety from the frontend response
+  const createdUser = await User.findById(user._id).select(
+    "-password -refreshToken"
+  );
 
- if (!createdUser) {
-  throw new ApiError(500, "Something went wrong while registering the user")
- }
+  if (!createdUser) {
+    throw new ApiError(500, "Something went wrong while registering the user");
+  }
 
- return res.status(201).json(
-  new ApiResponse(200, createdUser, "User registered successfully")
- )
+  return res
+    .status(201)
+    .json(new ApiResponse(200, createdUser, "User registered successfully"));
 });
 
 export { registerUser };
