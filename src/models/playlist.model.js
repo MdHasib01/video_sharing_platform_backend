de1f2560da5b6c445e3c4ecd7493d3e064921858
MdHasib01 <md.hasibuzzaman001@gmail.com>
2024-04-01 12:10:17 +0600

    add comment, like, tweet, playlist  models

---
+++ src/models/playlist.model.js
@@ -0,0 +1,27 @@
+import mongoose, { Schema } from "mongoose";
+
+const playListSchema = new Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    description: {
+      type: String,
+      required: true,
+    },
+    owner: {
+      type: Schema.Types.ObjectId,
+      ref: "User",
+    },
+    videos: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: "Video",
+      },
+    ],
+  },
+  { timestamps: true }
+);
+
+export const PlayList = mongoose.model("PlayList", playListSchema);
